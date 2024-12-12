abb578262394a5175b1e2513842f7a93ac04ccaa
Jakub Jucha <kuba.jucha.24@gmail.com>
2024-12-12 12:22:07 +0100

    rename functions, add config stucture

--- Project/drivers/TP/TP.c
+++ Project/drivers/TP/TP.c
@@ -1,5 +1,13 @@
 #include "TP.h"
 
-int TP_get_X() { return touchpanelReadX(); }
-int TP_get_Y() { return touchpanelReadY(); }
-void TP_get_XY(int *x, int *y) { touchpanelGetXY(x, y); }
+void TP_initialize() { touchpanelInit(); }
+void TP_set_bounds(int min_x, int max_x, int min_y, int max_y) {
+  TP_config.MIN_READ_X = min_x;
+  TP_config.MAX_READ_X = max_x;
+  TP_config.MIN_READ_Y = min_y;
+  TP_config.MAX_READ_Y = max_y;
+}
+
+int TP_read_X() { return touchpanelReadX(); }
+int TP_read_Y() { return touchpanelReadY(); }
+void TP_read_XY(int *x, int *y) { touchpanelGetXY(x, y); }
--- Project/drivers/TP/TP.h
+++ Project/drivers/TP/TP.h
@@ -3,8 +3,22 @@
 
 #include "TP_Open1768.h"
 
-int TP_get_X();
-int TP_get_Y();
-void TP_get_XY(int *x, int *y);
+struct {
+  int MIN_READ_X;
+  int MAX_READ_X;
+
+  int MIN_READ_Y;
+  int MAX_READ_Y;
+} TP_config;
+
+/// TODO: write interrupt with average read
+/// TODO: flag read ready
+
+void TP_initialize();
+void TP_set_bounds(int min_x, int max_x, int min_y, int max_y);
+
+int TP_read_X();
+int TP_read_Y();
+void TP_read_XY(int *x, int *y);
 
 #endif
