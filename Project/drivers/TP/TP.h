f3e746d82d9ba43d5dcd05ed52027a3eb0c873fa
BartlomiejRydzak <152024463+BartlomiejRydzak@users.noreply.github.com>
2024-12-19 12:01:05 +0100

    Update TP.h

--- Project/drivers/TP/TP.h
+++ Project/drivers/TP/TP.h
@@ -3,13 +3,18 @@
 
 #include "TP_Open1768.h"
 
-struct {
+static struct {
   int MIN_READ_X;
   int MAX_READ_X;
 
   int MIN_READ_Y;
   int MAX_READ_Y;
+  int read;
 } TP_config;
+
+
+void TP_IRQ_callback();
+void TP_IRQ_initialize();
 
 /// TODO: write interrupt with average read
 /// TODO: flag read ready
@@ -20,5 +25,7 @@ void TP_set_bounds(int min_x, int max_x, int min_y, int max_y);
 int TP_read_X();
 int TP_read_Y();
 void TP_read_XY(int *x, int *y);
-
+void TP_touched(int* x, int* y);
+void handle_touch(uint16_t color);
+void draw_ship(uint16_t x, uint16_t y, uint16_t color);
 #endif
